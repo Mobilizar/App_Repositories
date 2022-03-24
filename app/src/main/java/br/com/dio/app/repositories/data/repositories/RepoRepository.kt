664112f7892f051372e7c4045fe88cafb5b74ace
RaycksonCosta <raycksonaraujo@gmail.com>
2022-03-24 13:59:06 -0300

    App Concluido

--- app/src/main/java/br/com/dio/app/repositories/data/repositories/RepoRepository.kt
+++ app/src/main/java/br/com/dio/app/repositories/data/repositories/RepoRepository.kt
@@ -3,7 +3,6 @@ package br.com.dio.app.repositories.data.repositories
 import br.com.dio.app.repositories.data.model.Repo
 import kotlinx.coroutines.flow.Flow
 
-
 interface RepoRepository {
     suspend fun listRepositories(user: String): Flow<List<Repo>>
 }
--- app/src/main/java/br/com/dio/app/repositories/ui/MainActivity.kt
+++ app/src/main/java/br/com/dio/app/repositories/ui/MainActivity.kt
@@ -3,21 +3,48 @@ package br.com.dio.app.repositories.ui
 import android.os.Bundle
 import android.util.Log
 import android.view.Menu
-import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.SearchView
 import br.com.dio.app.repositories.R
+import br.com.dio.app.repositories.core.createDialog
+import br.com.dio.app.repositories.core.createProgressDialog
+import br.com.dio.app.repositories.core.hideSoftKeyboard
 import br.com.dio.app.repositories.databinding.ActivityMainBinding
+import br.com.dio.app.repositories.presentation.MainViewModel
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : AppCompatActivity(), SearchView.OnQueryTextListener {
 
+    private val viewModel by viewModel<MainViewModel>()
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    private val adapter by lazy { RepoListAdapter() }
+    private val dialog by lazy { createProgressDialog() }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
         setSupportActionBar(binding.toolbar)
+        binding.rvRepo.adapter = adapter
+
+        viewModel.getRepoList("Mobilizar")
+        viewModel.repos.observe(this) {
+            when (it) {
+                MainViewModel.State.Loading -> {
+                    dialog.show()
+                }
+                is MainViewModel.State.Error -> {
+                    createDialog {
+                        setMessage(it.error.message)
+                    }.show()
+                    dialog.dismiss()
+                }
+                is MainViewModel.State.Successes -> {
+                    dialog.dismiss()
+                    adapter.submitList(it.list)
+                }
+            }
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -28,7 +55,8 @@ class MainActivity : AppCompatActivity(), SearchView.OnQueryTextListener {
     }
 
     override fun onQueryTextSubmit(query: String?): Boolean {
-        Log.e(TAG, "onQueryTextSubmit: $query")
+        query?.let { viewModel.getRepoList(it) }
+        binding.root.hideSoftKeyboard()
         return true
     }
 
